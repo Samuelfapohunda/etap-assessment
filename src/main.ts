6f2567f1afe98cea1823da923788726338ebcfe7
samuel fapohunda <samzyfaps@gmail.com>
2024-09-23 19:07:51 +1300

    cors update

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   setupSwagger(app);
   await app.listen(3000);
 
-  app.enableCors();
 }
 
 bootstrap();
